bc7e80238f6ab522c9e2a3b62dd80b956538eb03
plut092 <johan.m.engstrom92@gmail.com>
2019-01-30 21:57:50 +0100

    init commit

--- Lab2/files-lab2/sieves-heap.c
+++ Lab2/files-lab2/sieves-heap.c
@@ -17,58 +17,46 @@ void print_number(int n) {
   }
 }
 
-void print_sieves(int n, bool *prime_list){
-  // init bool array (true)
-  printf("%d\n", n); ///////////////////////////////////////
-  // bool *prime_list = (bool *) malloc(n * sizeof(bool));
-  printf("%d\n", sizeof(*prime_list)); /////////////////////
-  prime_list[0] = false;
-  prime_list[1] = false;
-
-  for (int i = 2; i <= n; i++) {
-    prime_list[i] = true;
+void print_sieves(int n){
+  // init bool heap with value true, 2 up to n (indicies are -2 of the number)
+  bool *prime_list = (bool *) malloc(n);
+  for (int i = 0; i < n; i++) {
+    prime_list[i] = 1;
   }
-  // algoritm Sieve of Eratosthenes
+  // algoritm Sieve of Eratosthenes - indicies are -2 of the number
   for (int i = 2; i <= sqrt(n); i++) {
-    if (prime_list[i]) {
+    if (prime_list[i - 2]) {
       for (int j = i * i; j <= n; j += i) {
-        prime_list[j] = false;
+        prime_list[j - 2] = 0;
       }
     }
   }
   // print primes up to n
   for (int i = 2; i <= n; i++) {
-    if (prime_list[i]) {
+    if (prime_list[i - 2]) {
       print_number(i);
     }
   }
-  // free(prime_list); /////////////////////////////////////////
+  free(prime_list);
 }
 
 int main(int argc, char *argv[]){
-  printf("ARGUMENT: %s\n", argv[1]);
-
+  // start program timer for time_spent executing program
   clock_t begin = clock();
-
-
-  if(argc == 2){
-    bool *heap = (bool*) malloc(atoi(argv[1]) * sizeof(bool));
-    print_sieves(atoi(argv[1]), heap);
-    free(heap);
-  }
-
+  if(argc == 2)
+    print_sieves(atoi(argv[1]));
   else
     printf("Please state an interger number.\n");
+
+    // calculating and printing time_spent on execution of program
     clock_t end = clock();
     double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("\nExecution time:\t%f\n", time_spent);
+    printf("\nExecution time: %.10f s\n", time_spent);
+
   return 0;
 }
-/* sieves:
-within 2 seconds:  n = 770 000
-within 10 seconds: n = ~2 044 000 more gives
-                       segmentation fault (out of memory on stack)
-sieves-heap:
-within 2 seconds:  n =
-within 10 seconds: n =
-*/
+
+// within 2 seconds:  n =    770 000
+// within 10 seconds: n =  2 044 000 (segmentation fault (out of memory on stack))
+// within 2 seconds:  n =  4 200 000
+// within 10 seconds: n = 21 500 000
