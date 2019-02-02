72a442a3612ebd0216246b2dc9dbe1cf1a189a2a
plut092 <johan.m.engstrom92@gmail.com>
2019-02-02 18:37:43 +0100

    lab2ass4 complete

--- Lab2/files-lab2/pointers.asm
+++ Lab2/files-lab2/pointers.asm
@@ -35,18 +35,17 @@ work:
 	PUSH	($ra)
 	# loading arguments for copycodes
 	la 	$a0,text1 # arg1 pointer to string text1
-	la	$a1,list1	# arg2 char[] list1
-	la	$a2,count	# arg3 <datatype> count (int?)
+	la	$a1,list1	# arg2 int[] list1
+	la	$a2,count	# arg3 count int
 	jal	copycodes	# calling copycodes
 	# <void> copycodes has no return ($v0)
 	la 	$a0,text2	# arg1 pointer to string text2
 	la	$a1,list2	# arg2 char[] list2
-	la	$a2,count	# arg3 <datatype> count
+	la	$a2,count	# arg3 count int
 	jal	copycodes
 	# <void> copycodes has no return ($v0)
 	# <void> work has no return ($v0)
 	POP	($ra)
-
 
 # function copycodes()
 copycodes:
@@ -54,7 +53,7 @@ copycodes:
 loop:
 	lb	$t0,0($a0)	# store char from string text
 	beq	$t0,$0,done # while there is chars in text
-	sw	$t0,0($a1)	# store word to list
+	sw	$t0,0($a1)	# store word with char to list
 
 	addi	$a0,$a0,1	# move to next byte (char) in string
 	addi	$a1,$a1,4	# move to next word in list
